46c365bd2416a26a062cff4fa3f1b355bcdde859
alopes-n <alex.lopes898@gmail.com>
2022-04-22 14:19:24 +0200

    C_00

---
+++ c_00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alopes-n <alopes-n@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/08 00:20:34 by alopes-n          #+#    #+#             */
+/*   Updated: 2022/04/08 00:23:03 by alopes-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	write (1, "zyxwvutsrqponmlkjihgfedcba", 26);
+}
